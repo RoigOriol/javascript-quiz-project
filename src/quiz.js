54487d43f1beafbe5a4c14e95631c978ccd70f26
Alfonso <alfonsomanuel2000@gmail.com>
2024-04-15 18:04:34 +0200

    Day 1 almost finished

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,41 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor (questions, timeLimit, timeRemaining){
+        this.questions = questions
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
 
-    // 2. getQuestion()
+    getQuestion(){
+        return this.questions[this.currentQuestionIndex]
+    }
     
-    // 3. moveToNextQuestion()
+    moveToNextQuestion(){
+        return this.currentQuestionIndex++
+    }
 
-    // 4. shuffleQuestions()
+     shuffleQuestions() {
+        for (let i = this.questions.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            let temp = this.questions[i];
+            this.questions[i] = this.questions[j];
+            this.questions[j] = temp;
+        }
+      }
 
-    // 5. checkAnswer(answer)
+    checkAnswer(answer){
+        if ( answer === this.currentQuestionIndex ){
+            this.correctAnswers + 1;
+        }
 
-    // 6. hasEnded()
-}
+    }
+
+    hasEnded(){
+       if (this.currentQuestionIndex < this.questions.length){
+        return false
+       } else {
+        return true
+       }
+    }
+    }
